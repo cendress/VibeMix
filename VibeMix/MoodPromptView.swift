ce2eb06939f70c9a02532d430c482bac254bc222
cendress <95492688+cendress@users.noreply.github.com>
2024-03-23 09:45:22 -0400

    Implement an enum for mood options

--- VibeMix/MoodPromptView.swift
+++ VibeMix/MoodPromptView.swift
@@ -7,9 +7,30 @@
 
 import SwiftUI
 
+/* CaseIterable allows all items within a type to be iterated over in a similar behavior to an array */
+enum MoodOption: String, CaseIterable {
+  case happy = "Happy"
+  case sad = "Sad"
+  case energetic = "Energetic"
+  case relaxed = "Relaxed"
+  
+  var description: String {
+    switch self {
+    case .happy:
+      return "I'm feeling happy and upbeat."
+    case .sad:
+      return "I'm feeling a bit down."
+    case .energetic:
+      return "I'm ready to take on the world!"
+    case .relaxed:
+      return "I'm in a chill, laid-back mood."
+    }
+  }
+}
+
 struct MoodPromptView: View {
   var body: some View {
-    Text("Question prompts will be here")
+    Text("Hello")
   }
 }
 
