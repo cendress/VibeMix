0f3198dd870773acf94ba91304adb6329527dab0
cendress <95492688+cendress@users.noreply.github.com>
2024-03-23 08:41:44 -0400

    Initial Commit

---
+++ VibeMix/VibeMixApp.swift
@@ -0,0 +1,20 @@
+//
+//  VibeMixApp.swift
+//  VibeMix
+//
+//  Created by Christopher Endress on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct VibeMixApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
