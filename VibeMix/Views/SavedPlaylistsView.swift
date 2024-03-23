c1fafdb21308863db9da8c273617096545a5767c
cendress <95492688+cendress@users.noreply.github.com>
2024-03-23 15:58:19 -0400

    Implement views

---
+++ VibeMix/Views/SavedPlaylistsView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedPlaylistView.swift
+//  VibeMix
+//
+//  Created by Christopher Endress on 3/23/24.
+//
+
+import SwiftUI
+
+struct SavedPlaylistsView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  SavedPlaylistsView()
+}
