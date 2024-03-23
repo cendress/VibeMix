66c626305b110b7a14dbf5561d05c2ba7db6c42c
cendress <95492688+cendress@users.noreply.github.com>
2024-03-23 16:00:00 -0400

    Implement titles for views

--- VibeMix/Views/SettingsView.swift
+++ VibeMix/Views/SettingsView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct SettingsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    NavigationView {
+      Text("Settings view")
     }
+    .navigationTitle("Settings")
+  }
 }
 
 #Preview {
-    SettingsView()
+  SettingsView()
 }
