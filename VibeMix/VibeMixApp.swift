360289be3370fd003b9d380df3436d07147acc38
cendress <95492688+cendress@users.noreply.github.com>
2024-03-27 15:23:54 -0400

    Add comments

--- VibeMix/VibeMixApp.swift
+++ VibeMix/VibeMixApp.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 @main
 struct VibeMixApp: App {
+  // Shared instance of PersistenceController
   let persistenceController = PersistenceController.shared
   
   var body: some Scene {
     WindowGroup {
       ContentView()
+      // Inject the managed object context into the environment of ContentView
+      // This allows ContentView and any of its children to access the managed object context for Core Data operations
         .environment(\.managedObjectContext, persistenceController.container.viewContext)
     }
   }
