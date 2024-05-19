96416c837899200fc5a1d5c63400fa9e714fb8db
cendress <95492688+cendress@users.noreply.github.com>
2024-05-19 12:17:02 -0400

    Remove background color

--- VibeMix/Views/MoodPromptView.swift
+++ VibeMix/Views/MoodPromptView.swift
@@ -54,83 +54,80 @@ struct MoodPromptView: View {
   
   var body: some View {
     NavigationView {
-      ZStack {
-        Color("BackgroundColor").ignoresSafeArea()
-        VStack {
-          Spacer()
-          Spacer()
-          Text("How are you feeling?")
-            .font(.title)
-            .padding(.bottom, 20)
-          
-          // LazyVGrid for creating a grid layout
-          LazyVGrid(columns: columns, spacing: 20) {
-            ForEach(MoodOption.allCases, id: \.self) { mood in
-              Button(action: {
-                // Set the selected mood and reset button selection when buttons are tapped
-                self.selectedMood = mood
-                self.isButtonSelected = false
-              }) {
-                VStack {
-                  Image(systemName: mood.symbol)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 50, height: 50)
-                  
-                  Text(mood.description)
-                    .multilineTextAlignment(.center)
-                }
-                .padding()
-                // Make the button width fill the available space
-                .frame(maxWidth: .infinity, maxHeight: 200)
-                // Change the background and text color based on selection
-                .background(self.selectedMood == mood ? Color("AppColor") : Color.white)
-                .foregroundColor(self.selectedMood == mood ? Color.white : Color("AppColor"))
-                .cornerRadius(25)
+      VStack {
+        Spacer()
+        Spacer()
+        Text("How are you feeling?")
+          .font(.title)
+          .padding(.bottom, 20)
+        
+        // LazyVGrid for creating a grid layout
+        LazyVGrid(columns: columns, spacing: 20) {
+          ForEach(MoodOption.allCases, id: \.self) { mood in
+            Button(action: {
+              // Set the selected mood and reset button selection when buttons are tapped
+              self.selectedMood = mood
+              self.isButtonSelected = false
+            }) {
+              VStack {
+                Image(systemName: mood.symbol)
+                  .resizable()
+                  .aspectRatio(contentMode: .fit)
+                  .frame(width: 50, height: 50)
+                
+                Text(mood.description)
+                  .multilineTextAlignment(.center)
               }
-              // Style the button with a border and rounded corners
-              .overlay(
-                RoundedRectangle(cornerRadius: 20)
-                  .stroke(selectedMood == mood ? Color.clear : Color("AppColor"), lineWidth: 2)
-              )
+              .padding()
+              // Make the button width fill the available space
+              .frame(maxWidth: .infinity, maxHeight: 200)
+              // Change the background and text color based on selection
+              .background(self.selectedMood == mood ? Color("AppColor") : Color.white)
+              .foregroundColor(self.selectedMood == mood ? Color.white : Color("AppColor"))
+              .cornerRadius(25)
             }
+            // Style the button with a border and rounded corners
+            .overlay(
+              RoundedRectangle(cornerRadius: 20)
+                .stroke(selectedMood == mood ? Color.clear : Color("AppColor"), lineWidth: 2)
+            )
           }
-          .padding(.horizontal, 20)
-          
-          Spacer()
-          
-          // Create a "Find My Vibe" button that is enabled only if a mood is selected
-          Button(action: {
-            // Set isButtonSelected to true if a mood is selected to trigger navigation
-            if self.selectedMood != nil {
-              self.isButtonSelected = true
-            }
-          }) {
-            HStack {
-              Image(systemName: "music.note.list")
-              Text("Find My Vibe")
-            }
-            .frame(maxWidth: .infinity)
-            .padding()
-            .background(Color("AppColor"))
-            .foregroundColor(.white)
-            .cornerRadius(10)
-          }
-          .padding(.horizontal, 100)
-          .disabled(selectedMood == nil)
-          
-          // Create a NavigationLink that is activated when isButtonSelected is true
-          if let selectedMood = selectedMood {
-            NavigationLink(destination: PlaylistView(mood: selectedMood), isActive: $isButtonSelected) {
-              EmptyView()
-            }
-          }
-          
-          Spacer()
-          Spacer()
         }
-        .navigationTitle("New Playlist")
+        .padding(.horizontal, 20)
+        
+        Spacer()
+        
+        // Create a "Find My Vibe" button that is enabled only if a mood is selected
+        Button(action: {
+          // Set isButtonSelected to true if a mood is selected to trigger navigation
+          if self.selectedMood != nil {
+            self.isButtonSelected = true
+          }
+        }) {
+          HStack {
+            Image(systemName: "music.note.list")
+            Text("Find My Vibe")
+          }
+          .frame(maxWidth: .infinity)
+          .padding()
+          .background(Color("AppColor"))
+          .foregroundColor(.white)
+          .cornerRadius(10)
+        }
+        .padding(.horizontal, 100)
+        .disabled(selectedMood == nil)
+        
+        // Create a NavigationLink that is activated when isButtonSelected is true
+        if let selectedMood = selectedMood {
+          NavigationLink(destination: PlaylistView(mood: selectedMood), isActive: $isButtonSelected) {
+            EmptyView()
+          }
+        }
+        
+        Spacer()
+        Spacer()
       }
+      .navigationTitle("New Playlist")
     }
   }
 }
