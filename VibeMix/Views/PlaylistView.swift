d25bd46346151b4d1ccdd192eb780bb8128627cb
cendress <95492688+cendress@users.noreply.github.com>
2024-05-19 13:37:39 -0400

    Create a new variable to track when to trigger navigation

--- VibeMix/Views/PlaylistView.swift
+++ VibeMix/Views/PlaylistView.swift
@@ -16,6 +16,7 @@ struct PlaylistView: View {
   @State private var audioPlayer: AVAudioPlayer?
   @State private var playlistName = ""
   @State private var showingNameSheet = false
+  @State private var navigateToSavedPlaylists = false
   
   @Environment(\.managedObjectContext) private var viewContext
   
