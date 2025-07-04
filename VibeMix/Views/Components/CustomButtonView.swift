9801b9d89513aad404cc851e3102ca53f791dfdb
cendress <95492688+cendress@users.noreply.github.com>
2025-07-04 15:37:17 -0400

    Create CustomButtonView for better modularity

---
+++ VibeMix/Views/Components/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonView.swift
+//  VibeMix
+//
+//  Created by Christopher Endress on 7/4/25.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+}
